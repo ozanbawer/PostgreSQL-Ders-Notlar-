f7e80ae54ea5f2edad527b58ca92d8a1bef3904e
OzanBawer <ozanb@aof.anadolu.edu.tr>
2026-01-17 16:04:08 +0300

    first commit

---
+++ dersnotlari.sql
@@ -0,0 +1,118 @@
+SELECT * FROM film;   				
+-- Bütün tabloları getiren sorgu
+
+SELECT title,description FROM film;   
+-- Belirli bir tablo/tabloları getiren sorgu  
+-- NOT:büyük küçük harf duyarlılığı için tırnaklı "column" şeklinde yazılmalı 
+
+SELECT first_name || ' ' || last_name AS full_name FROM actor;   
+-- AS(ALIAS) yeni_kolon_ismi  için kullanılır
+-- || kolonları birleştirmek için kullanılır yerine => CONCAT(first_name, ' ', last_name) de kullanılabilir.
+
+SELECT UPPER(first_name) FROM actor;
+-- UPPER(kolonismi) o kolondaki verileri büyük harf yapar, LOWER() küçük harf yapar.
+
+SELECT SUBSTRING(title, 1, 5) FROM film;
+-- SUBSTRING (kolon ismi, ilk harakter, son karakter) 
+-- => o tablodaki her satırda belirlenen sayıdaki karakterleri getiriyor UPPER ve LOWER ile kullanılabilir
+
+SELECT LENGTH(title) FROM film; 
+--LENGTH(kolonismi) => tablodaki her satırın karakter uzunluğunu verir.
+
+SELECT title,rating FROM film
+WHERE rating = 'G';  "WHERE" koşullar belirlemek için kullanılır 
+-- < küçüktür , > büyüktür , >= büyükeşit, <= küçük eşit , <> ve != eşit değildir 
+
+SELECT * FROM film
+WHERE length BETWEEN 50 AND 100;
+--"BETWEEN value1 AND value2" belirli bir aralıktaki verileri göstermek için kullanılır  (stringlerde de kullanılır)
+--"NOT BETWEEN value1 AND value2" NOT kullanılırsa o aralıkta olmayanları getirir koşulun tersini alır
+
+SELECT * FROM film
+WHERE rental_rate IN (0.99, 2.99);
+-- "IN (value1,value2,...)" belirli değerlerin içinde olduğu verileri göstermek için kullanılır
+-- "NOT IN" kullanılırsa o değer olmayanları getirir koşulun tersini alır
+
+SELECT * FROM film
+WHERE rental_rate IN (2.99, 0.99) AND "length" BETWEEN 100 AND 130
+-- İki koşul arasına "AND" gelirse her iki koşulunda sağlandığı bir sorgu oluşur 
+--(Hepsi gerçekleşmek zorunda)
+
+SELECT * FROM film
+WHERE rental_rate = 0.99 OR rating = 'PG';
+-- İki koşul arasına "OR" gelirse her iki koşuldan her birinin ayrı ayrı sağlandığı bir sorgu oluşur (Biri gerçekleşse yeterli)
+
+/* AND ve OR için "NOT" kullanılırsa o koşulun sağlanmadığı sorgulama yapılır ve gösterilir
+ AND sorgulamalarda OR'a göre daha önceliklidir dikkat edilmesi gerekiyor !!!! */
+
+SELECT * FROM film
+WHERE title LIKE 'B%'; 
+--LIKE anahtar kelimesi ile bir karakteri bilinen veri/veriler getirilir
+-- %  0 veya daha fazla karakter için kullanılır _ ile sadece bir karakter için kullanılır 
+-- B ile başlayıp içinde M olaran ve % olan yerlerde de 0 veya daha fazla karakter için LIKE 'B%M%'   
+--'B_' ile başlayı sonrasında sadece 1 karakter için LIKE 'B_'
+--% veya _ nerede olduğu o yerdeki karakteri temsil eder başta sonra ortada farketmez ve beraberde kullanılır LIKE 'C_%o'
+-- LIKE büyük küçük harf duyarlıdır
+
+SELECT * FROM film
+WHERE title ILIKE 'b%H'; 
+--LIKE anahtar kelime ile aynı özelliklerdedir fakat büyük küçük harf duyarlılığı yoktur 
+
+/* NOT LIKE veya NOT ILIKE kullanıldığında bu şartı sağlamayanlar getirilir */
+
+SELECT * FROM film
+WHERE title SIMILAR TO 'B%|C%';
+-- SIMILAR TO LIKE gibi çalışır ama farklı özellikleri var örn: | veya anlamında daha fazla seçenek sağlar (döküman incele)
+
+SELECT * FROM film
+WHERE title SIMILAR TO '[A-Z,a-z]{5}'; 
+--Bu özellik ile a'danz'ye büyük küçük farketmeksizin 5 karakterli olanları getirir
+--A,Z ve 5 değiştirildiğinde ona göre veriler getirilir {5,7} olursa 5den 7ye kadar olan karakterler gelir
+
+SELECT COUNT(*) FROM customer;
+-- COUNT sütun sayısını toplayan bir özelliktir (Boş sütunları saymaz veri olanları sayar)
+-- Koşullara bağlı olan sütunların sadece sayısını gösterir
+
+SELECT DISTINCT rental_rate FROM film;
+--DISTINCT anahtar kelimesi benzersiz olanları listeler 
+
+SELECT * FROM film
+ORDER BY "length" DESC;
+-- ORDER BY .... DESC (ASC) verileri sıralar sayıları büyük-küçük harfleri a-z olarak
+-- DESC büyükten küçüğe (z-a) ASC küçükten büyüğe (a-z) (NOT: NULL veriler en büyükten sonra veya zden sonra gibi işlenir)
+
+SELECT * FROM film
+ORDER BY "length" ASC
+LIMIT 10
+--LIMIT sayı; verilerin ilk sayı kadarını getirir
+OFFSET 5
+--OFFSET sayı; verilerin ilk sayı kadarını atlar öyle getirir
+/*OFFSET VE LIMIT BERABERDE KULLANILABİLİR AYRI AYRIDA */
+
+
+SELECT AVG("length"), MAX("length"), MIN ("length"), SUM("length") 
+FROM film;
+--AVG: sütunun ortalamasını alır, MAX: sütunda en büyük olanı alır, MIN: sütunda en küçük olanı alır, SUM: bütün sütunun toplamını alır */
+
+
+SELECT rental_rate, MAX("length") FROM film
+GROUP BY rental_rate;
+--GROUP BY column1 anahtar ifadesi kullanıldığı sütunu gruplar ve fonsiyonlar ile kullanılınca anlamlı çıktı verir*/
+
+/* GENEL TEKRAR*/
+SELECT customer_id, SUM(amount) AS total_amount FROM payment -- default syntax (AS yeni sütuna isim verme)
+WHERE customer_id < 100 -- sütun koşulları belirtmek için
+GROUP BY customer_id -- sütuna göre gruplama
+HAVING SUM(amount) > 100 -- gruplanan sütunların değerleri üzerinden koşul (sadece yeni değerlere göre uygular)
+ORDER BY total_amount -- azdan çoğa (veya çoktan aza) sırlama
+LIMIT 10 -- gösterilecek veri sayısını limitlendirme
+OFFSET 5; -- baştan atlanacak veri sayısını belirtme
+/* GENEL TEKRAR*/
+
+
+
+
+
+
+
+
